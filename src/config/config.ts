85b6ca809b9dbf0c2c7368884074f59b7c2567ce
ananyamittal1716-bit <ananyamittal1716@gmail.com>
2026-02-02 12:48:17 +0530

    HIIIIUUUU

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ export const getAsset = (path: string) => {
 const siteConfig = {
   personal: {
     name: 'ANANYAAA MITALL',
-    title: 'Your Title | Your Role',
+    title: 'DANCER  | Your Role',
     image: getAsset('images/profile.png'), // Customize or replace with your profile image
     description:
       'Write a short bio here describing your background and focus. Keep it to 2–4 sentences.\n' +
